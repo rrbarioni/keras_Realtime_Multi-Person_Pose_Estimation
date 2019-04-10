9fc91a563f247370c038c683f46e66c2a906542f
Ricardo <rrb@cin.ufpe.br>
2019-04-10 18:05:56 -0300

    doing color stuff

--- egocap_omni_renderer.py
+++ egocap_omni_renderer.py
@@ -113,20 +113,6 @@ class Util:
     def fold(f, l, a):
         return a if (len(l) == 0) else Util.fold(f, l[1:], f(a, l[0]))
 
-class GroundTruth:
-    def __init__(self, joints):
-        self.joints = joints
-        self.bones_size = {
-            bone_name: self.joints[bone_edges[0]].distance(
-                self.joints[bone_edges[1]])
-            for (bone_name, bone_edges) in bones_list.items() }
-
-    def __str__(self):
-        s = ''
-        for bone_name, bone_value in self.bones_size.items():
-            s += str(bone_name) + ': ' + str(bone_value) + '\n'
-        return s
-
 class Camera:
     def __init__(self, horizontal_fov, vertical_fov, fx, fy, cx, cy):
         self.fx = fx
@@ -156,14 +142,14 @@ class Camera:
             for (key,value) in screen_joints.items() })
 
 class Render:
-    def __init__(self, width, height, camera, screen_world_joints, joints):
+    def __init__(self, width, height, camera, screen_world_joints, ground_truth):
         pygame.init()
         display = (width,height)
         pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
         self.camera = camera
         self.screen_world_joints = screen_world_joints
-        self.ground_truth = joints
+        self.ground_truth = ground_truth
 
         self.render_switches = {
             'camera': True,
@@ -173,11 +159,17 @@ class Render:
         }
 
         self.colors = {
-            'camera':                 (1,0.2,1),
-            'screen_world_joints':    (1,1,0),
-            'ground_truth':           (0,1,0),
-            'rays':                   (0.6,0.6,0.6),
-            'default':                (1,1,1)
+            'camera': (1, 0.2, 1),
+            'screen_world_joints': (1, 1, 0),
+            'ground_truth': (0, 1, 0),
+            'bones': [
+                (255,   0,   0), (255,  85,   0), (255, 170,   0), (255, 255,   0), (170, 255,   0),
+                ( 85, 255,   0), (  0, 255,   0), (  0, 255,  85), (  0, 255, 170), (  0, 255, 255),
+                (  0, 170, 255), (  0,  85, 255), (  0,   0, 255), ( 85,   0, 255), (170,   0, 255),
+                (255,   0, 255), (255,   0, 170), (255,   0,  85), (255,   0,   0)
+            ]
+            'rays': (0.6, 0.6, 0.6),
+            'default': (1, 1, 1)
         }
 
         self.front_vectors_size = 0.3
@@ -187,10 +179,10 @@ class Render:
         self.delta_mouse_pos = (0,0)
 
         gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
-        glTranslatef(0,0,-5)
-        glRotatef(90,0,1,0)
+        glTranslatef(0, 0, -5)
+        glRotatef(90, 0, 1, 0)
 
-        self.orientation = (0,0,-1)
+        self.orientation = (0, 0, -1)
 
     def set_camera(self, camera):
         self.camera = camera
@@ -341,13 +333,13 @@ class Render:
 
             self.draw_axis_origin()
 
-            if self.camera is not None and self.render_switches['camera']:
+            if self.render_switches['camera']:
                 self.draw_camera()
 
             if self.screen_world_joints is not None:
-                self.draw_rays(self.screen_world_joints.joints)
+                self.draw_rays(self.screen_world_joints)
                 self.draw_skeleton(
-                    self.screen_world_joints.joints, type='screen_world_joints')
+                    self.screen_world_joints, type='screen_world_joints')
 
             if self.ground_truth is not None:
                 self.draw_skeleton(self.ground_truth, type='ground_truth')
@@ -355,7 +347,68 @@ class Render:
             pygame.display.flip()
             pygame.time.wait(10)
 
+
 # test:
+# ORIGINAL:
+bones_list = {
+    'neck':          ('neck',       'head'),
+    'l_collar_bone': ('l_shoulder', 'neck'),
+    'l_upper_arm':   ('l_elbow',    'l_shoulder'),
+    'l_fore_arm':    ('l_wrist',    'l_elbow'),
+    'r_collar_bone': ('r_shoulder', 'neck'),
+    'r_upper_arm':   ('r_elbow',    'r_shoulder'),
+    'r_fore_arm':    ('r_wrist',    'r_elbow'),
+    'l_spine':       ('l_pelvis',   'neck'),
+    'l_femur':       ('l_knee',     'l_pelvis'),
+    'l_shin':        ('l_foot',     'l_knee'),
+    'r_spine':       ('r_pelvis',   'neck'),
+    'r_femur':       ('r_knee',     'r_pelvis'),
+    'r_shin':        ('r_foot',     'r_knee'),
+}
+
+screen_joints = {
+    'head':       Vector2d(948, 394),
+    'l_shoulder': Vector2d(858, 533),
+    'l_elbow':    Vector2d(824, 657),
+    'l_wrist':    Vector2d(824, 776),
+    'r_shoulder': Vector2d(1025, 532),
+    'r_elbow':    Vector2d(1041, 639),
+    'r_wrist':    Vector2d(1027, 744),
+    'l_pelvis':   Vector2d(895, 790),
+    'l_knee':     Vector2d(886, 957),
+    'l_foot':     Vector2d(874, 1107),
+    'r_pelvis':   Vector2d(977, 791),
+    'r_knee':     Vector2d(995, 948),
+    'r_foot':     Vector2d(1007, 1096),
+    'neck':       Vector2d(946, 473),
+}
+screen_world_joints = camera.get_screen_world_joints(screen_joints)
+
+# ground_truth = {
+#     "head":       Vector3d(-0.05539,  0.27005, 1.95791),
+#     "l_shoulder": Vector3d(-0.21802,  0.01493, 1.90445),
+#     "l_elbow":    Vector3d(-0.27991, -0.20893, 1.90757),
+#     "l_wrist":    Vector3d(-0.27114, -0.40407, 1.82395),
+#     "r_shoulder": Vector3d( 0.08004,  0.01386, 1.86651),
+#     "r_elbow":    Vector3d( 0.09393, -0.16023, 1.71716),
+#     "r_wrist":    Vector3d( 0.05694, -0.29506, 1.53766),
+#     "l_pelvis":   Vector3d(-0.15150, -0.43901, 1.87078),
+#     "l_knee":     Vector3d(-0.17072, -0.74263, 1.89426),
+#     "l_foot":     Vector3d(-0.19390, -1.01891, 1.91696),
+#     "r_pelvis":   Vector3d(-0.00689, -0.43827, 1.85139),
+#     "r_knee":     Vector3d( 0.02322, -0.71868, 1.87110),
+#     "r_foot":     Vector3d( 0.04517, -0.98975, 1.89516),
+#     "neck":       Vector3d(-0.05940,  0.12449, 1.95275),
+# }
+
+# EGOCAP:
+    # idx_in_egocap_str = ['Head', 'Neck', 'RShoulder', 'RElbow', 'RWrist',
+    #     'RFinger', 'LShoulder', 'LElbow', 'LWrist', 'LFinger', 'RHip', 'RKnee',
+    #     'RAnkle', 'RToe', 'LHip', 'LKnee', 'LAnkle', 'RToe']
+
+    # joint_pairs = list(zip(
+    #     [0, 1, 2, 3, 4, 1, 6, 7, 8, 1,  10, 11, 12, 1,  14, 15, 16],
+    #     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]))
 bones_list = {
     'neck':          ('neck',       'head'),
     'l_collar_bone': ('l_shoulder', 'neck'),
@@ -381,40 +434,4 @@ camera = Camera(
     cy=540.08,
 )
 
-screen_joints = {
-    'head':       Vector2d(948, 394),
-    'l_shoulder': Vector2d(858, 533),
-    'l_elbow':    Vector2d(824, 657),
-    'l_wrist':    Vector2d(824, 776),
-    'r_shoulder': Vector2d(1025, 532),
-    'r_elbow':    Vector2d(1041, 639),
-    'r_wrist':    Vector2d(1027, 744),
-    'l_pelvis':   Vector2d(895, 790),
-    'l_knee':     Vector2d(886, 957),
-    'l_foot':     Vector2d(874, 1107),
-    'r_pelvis':   Vector2d(977, 791),
-    'r_knee':     Vector2d(995, 948),
-    'r_foot':     Vector2d(1007, 1096),
-    'neck':       Vector2d(946, 473),
-}
-screen_world_joints = GroundTruth(camera.get_screen_world_joints(
-        screen_joints=screen_joints))
-
-ground_truth = GroundTruth({
-    "head":       Vector3d(-0.05539,  0.27005, 1.95791),
-    "l_shoulder": Vector3d(-0.21802,  0.01493, 1.90445),
-    "l_elbow":    Vector3d(-0.27991, -0.20893, 1.90757),
-    "l_wrist":    Vector3d(-0.27114, -0.40407, 1.82395),
-    "r_shoulder": Vector3d( 0.08004,  0.01386, 1.86651),
-    "r_elbow":    Vector3d( 0.09393, -0.16023, 1.71716),
-    "r_wrist":    Vector3d( 0.05694, -0.29506, 1.53766),
-    "l_pelvis":   Vector3d(-0.15150, -0.43901, 1.87078),
-    "l_knee":     Vector3d(-0.17072, -0.74263, 1.89426),
-    "l_foot":     Vector3d(-0.19390, -1.01891, 1.91696),
-    "r_pelvis":   Vector3d(-0.00689, -0.43827, 1.85139),
-    "r_knee":     Vector3d( 0.02322, -0.71868, 1.87110),
-    "r_foot":     Vector3d( 0.04517, -0.98975, 1.89516),
-    "neck":       Vector3d(-0.05940,  0.12449, 1.95275),
-})
-
-Render(1200, 900, camera, screen_world_joints, ground_truth.joints).run()
+Render(1200, 900, camera, screen_world_joints, None).run()
